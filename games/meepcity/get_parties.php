eb82617a04383e91a46136b01f55fbd9fcc3ed00
alosh <alosh@alosh.shop>
2023-12-30 03:43:17 -0600

    Update get_parties.php
    Signed-off-by: alosh <alosh@alosh.shop>

--- games/meepcity/get_parties.php
+++ games/meepcity/get_parties.php
@@ -2,10 +2,28 @@
   {
     "PartyId": 987654321,
     "PartyMaxPlayers": 5,
-    "PartyOwnerId": 123456789,
+    "PartyOwnerId": 673650206,
+    "PartyOwnerUsername": "alaooshh",
+    "PartyPlayersOnline": 3,
+    "PartyTier": 1,
+    "PartyTitle": "12 Gauge Shotgun"
+  }
+  {
+    "PartyId": 987654321,
+    "PartyMaxPlayers": 5,
+    "PartyOwnerId": 673650206,
     "PartyOwnerUsername": "alaooshh",
     "PartyPlayersOnline": 3,
     "PartyTier": 2,
-    "PartyTitle": "12 Gauge Shotgun"
-  }
+    "PartyTitle": "test2"
+  },
+  {
+    "PartyId": 987654321,
+    "PartyMaxPlayers": 5,
+    "PartyOwnerId": 673650206,
+    "PartyOwnerUsername": "alaooshh",
+    "PartyPlayersOnline": 3,
+    "PartyTier": 3,
+    "PartyTitle": "test3"
+  },
 ]
