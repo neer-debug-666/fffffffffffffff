6118e7594da1ee95a7dfb2ccacc376306f46d48e
alosh <alosh@alosh.shop>
2023-12-30 03:33:35 -0600

    my new years kiss is going to be a 12 gauge shotgun 😘
    Signed-off-by: alosh <alosh@alosh.shop>

--- games/meepcity/get_parties.php
+++ games/meepcity/get_parties.php
@@ -1,9 +1,13 @@
 {
-    "Response": [
-        { "PartyPlayersOnline": 3, "PartyMaxPlayers": 5, "PartyName": "Party 1" },
-        { "PartyPlayersOnline": 6, "PartyMaxPlayers": 8, "PartyName": "Party 2" },
-        { "PartyPlayersOnline": 2, "PartyMaxPlayers": 4, "PartyName": "Party 3" },
-        { "PartyPlayersOnline": 5, "PartyMaxPlayers": 6, "PartyName": "Party 4" },
-        { "PartyPlayersOnline": 1, "PartyMaxPlayers": 3, "PartyName": "Party 5" }
-    ]
+  "parties": [
+    {
+      "PartyOwnerId": 123456789,
+      "PartyOwnerUsername": "alaooshh",
+      "PartyTitle": "12 Gauge Shotgun",
+      "PartyTier": 2,
+      "PartyPlayersOnline": 3,
+      "PartyMaxPlayers": 5,
+      "PartyId": 987654321
+    }
+  ]
 }
