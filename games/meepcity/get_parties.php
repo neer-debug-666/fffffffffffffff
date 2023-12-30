35a731c8ace95c41a122d56cd356a8e88590561a
alosh <alosh@alosh.shop>
2023-12-30 03:31:40 -0600

    Hey im going insane
    Signed-off-by: alosh <alosh@alosh.shop>

--- games/meepcity/get_parties.php
+++ games/meepcity/get_parties.php
@@ -1,1 +1,9 @@
-aa
+{
+    "Response": [
+        { "PartyPlayersOnline": 3, "PartyMaxPlayers": 5, "PartyName": "Party 1" },
+        { "PartyPlayersOnline": 6, "PartyMaxPlayers": 8, "PartyName": "Party 2" },
+        { "PartyPlayersOnline": 2, "PartyMaxPlayers": 4, "PartyName": "Party 3" },
+        { "PartyPlayersOnline": 5, "PartyMaxPlayers": 6, "PartyName": "Party 4" },
+        { "PartyPlayersOnline": 1, "PartyMaxPlayers": 3, "PartyName": "Party 5" }
+    ]
+}
